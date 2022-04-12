aff301bbf7d5064fde9fada0a87f0e2f55f7856a
artginzburg <ginzburg.arthur@gmail.com>
2022-04-13 00:24:05 +0300

    setAll - don't throw on `undefined`

---
+++ src/setAll.test.js
@@ -0,0 +1,29 @@
+const test = require('ava');
+
+const { setAll } = require('./setAll');
+
+test('sets all keys of an object to the same value', (t) => {
+  const objectToMutate = {
+    first: 'anything',
+    second: 'different',
+    third: 3,
+    fourth: undefined,
+  };
+  const valueToSet = 'same';
+  setAll(objectToMutate, valueToSet);
+
+  t.deepEqual(objectToMutate, {
+    first: valueToSet,
+    second: valueToSet,
+    third: valueToSet,
+    fourth: valueToSet,
+  });
+});
+
+test("ignores (doesn't throw on) undefined", (t) => {
+  const objectToMutate = undefined;
+  const valueToSet = 'same';
+  setAll(objectToMutate, valueToSet);
+
+  t.is(objectToMutate, undefined);
+})
