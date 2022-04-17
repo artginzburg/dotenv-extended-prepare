29357fda753da3bc467d29d797d38d42e7f3d1a9
artginzburg <ginzburg.arthur@gmail.com>
2022-04-17 05:28:00 +0300

    add a TODO? comment about "\\n"

--- src/generateSchema.js
+++ src/generateSchema.js
@@ -48,6 +48,7 @@ async function findEnvVariables() {
       console.log(`Matching ${key} with RegExps...`);
 
       const destructured = element.matchAll(/\{(?<keyName>[^{}]*)\}\s*=\s*process\.env(.*)+$/gm);
+      // TODO? if this gives something like `\\n  NODE_ENV\\n` — that's means that the newlines are actually there in text, since we are using multiline RegExp. A good example — if you scan the Babel source map files. They often have `.json` extension. To solve this, we either need to exclude `.json` files from the scan, or use `.replace(/\\n/g, '')` to remove the newlines.
       for (const {
         groups: { keyName },
       } of destructured) {
